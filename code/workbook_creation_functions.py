63f16723498bec474030e39fc8eb49ee080b7197
Hyuga Kasai <hyuga.kasai@aperc.or.jp>
2024-02-13 17:52:03 +0900

    fixed x-axis years

--- code/workbook_creation_functions.py
+++ code/workbook_creation_functions.py
@@ -416,7 +416,7 @@ def format_table(table,plotting_names_order,plotting_name_to_label_dict):
     #format some cols:
     num_cols = len(table.columns)
     first_non_object_col = table.select_dtypes(exclude=['object']).columns[0]
-    year_cols_start = table.columns.get_loc(first_non_object_col) - 1
+    year_cols_start = max(0, table.columns.get_loc(first_non_object_col) - 1)
     
     #set order of columns and table, dependent on what the aggregate column is:
     year_cols = table.columns[year_cols_start:]
