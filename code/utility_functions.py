2ed385d6a41bfeb00ef00acf382a90a1008b935b
hyugakasai <hyuga817@gmail.com>
2024-06-27 17:10:39 +0900

    singapore

--- code/utility_functions.py
+++ code/utility_functions.py
@@ -6,7 +6,7 @@ import os
 from datetime import datetime
 
 
-ECONOMY_ID = '18_CT'#08_JPN
+ECONOMY_ID = '17_SGP'#08_JPN
 
 #######################################################
 #CONFIG PREPARATION
