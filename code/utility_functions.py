35e4669a4ae6812d8c89edafac782c2d0503d0c6
Hyuga Kasai <hyuga.kasai@aperc.or.jp>
2024-07-10 15:23:25 +0900

    BD

--- code/utility_functions.py
+++ code/utility_functions.py
@@ -6,7 +6,7 @@ import os
 from datetime import datetime
 
 
-ECONOMY_ID = '00_APEC'#08_JPN
+ECONOMY_ID = '02_BD'#08_JPN
 
 #######################################################
 #CONFIG PREPARATION
