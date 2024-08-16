60252c9d817f50f5ced2c504a7d3db443d896341
Hyuga Kasai <hyuga.kasai@aperc.or.jp>
2024-08-16 13:53:36 +0900

    CHL

--- code/utility_functions.py
+++ code/utility_functions.py
@@ -6,7 +6,7 @@ import os
 from datetime import datetime
 
 
-ECONOMY_ID = '12_NZ'#08_JPN
+ECONOMY_ID = '04_CHL'#08_JPN
 
 #######################################################
 #CONFIG PREPARATION
